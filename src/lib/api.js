b91fea408878a22f4a52bc672cd27b3f7e0a44af
webique-technology <webiquetech@gmail.com>
2026-05-25 17:13:53 +0530

    laravel api added in api.js

--- src/lib/api.js
+++ src/lib/api.js
@@ -3,7 +3,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "https://laravel.mahakumbhtourstravelsnashik.com/api",
 });
 
 export default API;
