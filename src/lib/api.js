1a429f7160f3e413f88807647d254ab8a470c5d1
webique-technology <webiquetech@gmail.com>
2026-05-25 13:03:02 +0530

    update the hotels and axios fetch file by aniket

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,10 +1,9 @@
+// api/axios.js
+
 import axios from "axios";
 
-const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
-  headers: {
-    "Content-Type": "application/json",
-  },
+const API = axios.create({
+  baseURL: "https://api.mahakumbhtourstravelsnashik.com/api",
 });
 
-export default api;
+export default API;
