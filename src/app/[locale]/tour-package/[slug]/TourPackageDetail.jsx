c472bf44bfa9723fe58fa19cf193347c119ea348
Anupam Shimpi <gymloverreact1997@gmail.com>
2026-06-01 11:58:46 +0530

    changes done by me

--- src/app/[locale]/tour-package/[slug]/TourPackageDetail.jsx
+++ src/app/[locale]/tour-package/[slug]/TourPackageDetail.jsx
@@ -5,9 +5,9 @@ import { Container, Row, Col } from "react-bootstrap";
 import { Link } from '@/i18n/routing';
 import Image from "next/image";
 import * as Icons from "lucide-react";
-import { Circle, FileText } from "lucide-react";
+import { Circle, FileText, CalendarCheck } from "lucide-react";
 import { motion } from "framer-motion";
-import { FaCalendarCheck } from "react-icons/fa";
+
 import { slugify, imageUrl } from "@/lib/utils";
 import { WhatsAppShareBtn } from "@/components/ui/button";
 import { HighlightsModal, TourTabs, } from "@/components/ui/common";
@@ -294,7 +294,7 @@ const TourPackageDetail = ({ tour }) => {
                                 href={`/tour-package/book/${slugify(tour.title)}`}
                                 className="primery-btn d-flex align-items-center justify-content-center gap-2 w-100 py-3 text-center text-decoration-none fw-bold rounded shadow-sm mb-3"
                             >
-                                <FaCalendarCheck size={18} />
+                                <CalendarCheck />
                                 Book Now
                             </Link>
 
